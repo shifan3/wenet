4b79cc9f5272ca7f6973fff74aa8225d2da76bcc
shi.fan <shi.fan@danatech.com>
2022-03-10 15:38:26 +0800

    qwe

--- runtime/server/x86/web/app.py
+++ runtime/server/x86/web/app.py
@@ -19,4 +19,4 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=args.port, debug=True, ssl_context=('cert.pem', 'key.pem'))
+    app.run(host='0.0.0.0', port=args.port, debug=False, ssl_context=('cert.pem', 'key.pem'), use_reloader=False)
