1a1f02b7c1650c4737797c6f1b8ffe831822b0b0
shi.fan <shi.fan@danatech.com>
2022-03-14 22:01:46 +0800

    qdqwd

--- runtime/core/decoder/torch_asr_decoder.cc
+++ runtime/core/decoder/torch_asr_decoder.cc
@@ -196,19 +196,22 @@ DecodeState TorchAsrDecoder::AdvanceDecoding() {
 }
 
 void TorchAsrDecoder::UpdateResult(bool finish) {
+  LOG(INFO) << "C1";
   const auto& hypotheses = searcher_->Outputs();
   const auto& inputs = searcher_->Inputs();
   const auto& likelihood = searcher_->Likelihood();
   const auto& times = searcher_->Times();
   result_.clear();
-
+  LOG(INFO) << "C2";
   CHECK_EQ_THROW(hypotheses.size(), likelihood.size());
   for (size_t i = 0; i < hypotheses.size(); i++) {
+    LOG(INFO) << "C3";
     const std::vector<int>& hypothesis = hypotheses[i];
 
     DecodeResult path;
     path.score = likelihood[i];
     int offset = global_frame_offset_ * feature_frame_shift_in_ms();
+    LOG(INFO) << "C4";
     for (size_t j = 0; j < hypothesis.size(); j++) {
       std::string word = symbol_table_->Find(hypothesis[j]);
       // A detailed explanation of this if-else branch can be found in
@@ -219,29 +222,37 @@ void TorchAsrDecoder::UpdateResult(bool finish) {
         path.sentence += (word);
       }
     }
-
+    LOG(INFO) << "C5";
     // TimeStamp is only supported in final result
     // TimeStamp of the output of CtcWfstBeamSearch may be inaccurate due to
     // various FST operations when building the decoding graph. So here we use
     // time stamp of the input(e2e model unit), which is more accurate, and it
     // requires the symbol table of the e2e model used in training.
     if (unit_table_ != nullptr && finish) {
+      LOG(INFO) << "C5.1";
       const std::vector<int>& input = inputs[i];
       const std::vector<int>& time_stamp = times[i];
       CHECK_EQ_THROW(input.size(), time_stamp.size());
+      LOG(INFO) << "C5.2";
       for (size_t j = 0; j < input.size(); j++) {
+        LOG(INFO) << "C5.3";
         std::string word = unit_table_->Find(input[j]);
         int start = j > 0 ? ((time_stamp[j - 1] + time_stamp[j]) / 2 *
                              frame_shift_in_ms())
-                          : 0;
+                          : 0;  
+        LOG(INFO) << "C5.4";
         int end = j < input.size() - 1 ? ((time_stamp[j] + time_stamp[j + 1]) /
                                           2 * frame_shift_in_ms())
                                        : offset_ * frame_shift_in_ms();
+        LOG(INFO) << "C5.5";
         WordPiece word_piece(word, offset + start, offset + end);
+        LOG(INFO) << "C5.6";
         path.word_pieces.emplace_back(word_piece);
       }
     }
+    LOG(INFO) << "C6";
     path.sentence = post_processor_->Process(path.sentence, finish);
+    LOG(INFO) << "C7";
     result_.emplace_back(path);
   }
 
