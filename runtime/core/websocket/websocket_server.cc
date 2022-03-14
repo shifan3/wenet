d9e0c587dcc2191219f0e7c55e94c91ee454568f
shi.fan <shi.fan@danatech.com>
2022-03-14 19:50:49 +0800

    adqw

--- runtime/core/websocket/websocket_server.cc
+++ runtime/core/websocket/websocket_server.cc
@@ -69,6 +69,7 @@ class ConnectionHandler {
             ws_.next_layer().set_verify_mode(asio::ssl::verify_none);
             ws_.next_layer().handshake(ssl::stream_base::server);
             ws_.accept();
+            throw check_error("")
             for (;;) {
                 // This buffer will hold the incoming message
                 beast::flat_buffer buffer;
